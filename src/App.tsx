5b5b36b9c619f47d274215ce19134051bbbe11e3
“[Martialeagle <k.l.ashwanthram@gmail.com>
2023-05-23 12:38:26 +0530

    added css

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">1232 +</div>
+        <div className="current-operand">89219821</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
